d2b029a41f208d49adbed492c6d8741a1c04f4ec
ChangChunHe <731176792@qq.com>
2018-10-26 15:12:09 +0800

    main file

---
+++ main.m
@@ -0,0 +1,8 @@
+clear
+clc
+pat = 'band';
+eigval_file = [pat, '/EIGENVAL'];
+dos_file = [pat, '/DOSCAR'];
+pos_file = [pat, '/POSCAR'];
+kpoint_file = [pat, '/KPOINTS'];
+draw_band_structure_dos(eigval_file, dos_file, pos_file, kpoint_file, 0, 16)
