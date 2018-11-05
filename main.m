5cc99773f4554ac1ab9418d52fe2870a32969dc5
ChangChunHe <731176792@qq.com>
2018-11-05 13:17:42 +0800

    add example

--- main.m
+++ main.m
@@ -5,4 +5,4 @@ eigval_file = [pat, '/EIGENVAL'];
 dos_file = [pat, '/DOSCAR'];
 pos_file = [pat, '/POSCAR'];
 kpoint_file = [pat, '/KPOINTS'];
-draw_band_structure_dos(eigval_file, dos_file, pos_file, kpoint_file, 0, 16)
+draw_band_structure_dos(eigval_file, dos_file, pos_file, kpoint_file,1, 80)
