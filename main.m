b7041f119e316cb7d2da7413b35785c698da4a29
ChangChunHe <731176792@qq.com>
2018-11-08 15:51:14 +0800

    modify bugs in help files

--- main.m
+++ main.m
@@ -6,10 +6,11 @@ eigval_file = fullfile(pat, 'EIGENVAL');
 dos_file = fullfile(pat, 'DOSCAR');
 pos_file = fullfile(pat, 'POSCAR');
 kpts_file = fullfile(pat, 'KPOINTS');
-% draw_band_structure_dos(eigval_file, dos_file, pos_file, kpts_file, have_pdos)
+%draw_band_structure_dos(eigval_file, dos_file, pos_file, kpts_file, have_pdos)
 
-% draw_band_structure(eigval_file, pos_file, kpts_file)
-% draw_dos_element(dos_file, pos_file)
-% draw_dos_pdos(dos_file, true)
-
+draw_band_structure(eigval_file, pos_file, kpts_file)
+draw_dos_element(dos_file, pos_file, true)
+axis([-6.5 4 -40 40])
+draw_dos_pdos(dos_file, true)
+axis([-6.5 4 -40 40])
 draw_band_structure_two_spin(eigval_file, pos_file, kpts_file)
