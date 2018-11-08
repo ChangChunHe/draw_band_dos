1b3cb51954bb13562f23ae41319fcf785bf1a48b
ChangChunHe <731176792@qq.com>
2018-11-08 13:56:46 +0800

    add some example

--- main.m
+++ main.m
@@ -1,8 +1,15 @@
 clear
 clc
-pat = 'band';
-eigval_file = [pat, '/EIGENVAL'];
-dos_file = [pat, '/DOSCAR'];
-pos_file = [pat, '/POSCAR'];
-kpoint_file = [pat, '/KPOINTS'];
-draw_band_structure_dos(eigval_file, dos_file, pos_file, kpoint_file,1, 80)
+pat = 'FM';
+have_pdos = true;
+eigval_file = fullfile(pat, 'EIGENVAL');
+dos_file = fullfile(pat, 'DOSCAR');
+pos_file = fullfile(pat, 'POSCAR');
+kpts_file = fullfile(pat, 'KPOINTS');
+% draw_band_structure_dos(eigval_file, dos_file, pos_file, kpts_file, have_pdos)
+
+% draw_band_structure(eigval_file, pos_file, kpts_file)
+% draw_dos_element(dos_file, pos_file)
+% draw_dos_pdos(dos_file, true)
+
+draw_band_structure_two_spin(eigval_file, pos_file, kpts_file)
