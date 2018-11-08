4e7470724872be96d8d419a3b703c2026ceb8267
ChangChunHe <731176792@qq.com>
2018-11-08 14:51:41 +0800

    read occupy number

--- draw_dos_element.m
+++ draw_dos_element.m
@@ -1,19 +1,18 @@
-function draw_dos_element(dos_file, pos_file)
+function draw_dos_element(dos_file, pos_file, have_pdos)
 %draw DOS of each element
 %   draw_dos_element(dos_file, pos_file)
 %   dos_file:   the path of DOSCAR file
 %   pos_file:   the path of pos file
+%   have_pdos: true or false
 %
 %   Examples:
 %       pos_file = 'FM/POSCAR';dos_file = 'FM/DOSCAR';
-%       draw_dos_element(dos_file, pos_file)
-%       axis([-5 3.5, -40 40])
+%       draw_dos_element(dos_file, pos_file, true)
+%       axis([-6.5 3.5, -40 40])
 %
 %
 %   See also  draw_dos_pdos, draw_band_structure, draw_specific_atoms
 
-
-have_pdos = 1;
 fid = fopen(dos_file, 'rt');
 k = 1;
 while feof(fid) == 0
@@ -51,9 +50,15 @@ if have_pdos
         for ik = 2:length(atom)
             seq(ik,:) = [num(ik-1)+1 sum(num(1:ik))];
         end
+        
+        [~,ind] = min(abs(sum_dos(:,1) - s(4)));
+        ind_up = find((sum_dos(:,4)-round(sum_dos(ind,4)))>0);
+        
+        ind_down =find((sum_dos(:,5)-round(sum_dos(ind,5)))>0);
+        
         figure
         hold on
-        sum_dos(:,1) = sum_dos(:,1) - s(4);
+        
         for ik = 1:length(atom)
             element_dos_up(:, ik) = sum(sum(p_dos(:,2:2:19,seq(ik,1):seq(ik,2)),2),3);
             element_dos_down(:, ik) = sum(sum(p_dos(:,3:2:19,seq(ik,1):seq(ik,2)),2),3);
@@ -61,16 +66,26 @@ if have_pdos
             plot(sum_dos(:,1), -element_dos_down(:,ik),'color', color_);
             plot(sum_dos(:,1), element_dos_up(:,ik), 'color',color_)
         end
-        ind = sum_dos(:,1) <= 0;
+        
         plot(sum_dos(:,1), sum_dos(:,2),'r','LineWidth', 2)
         plot(sum_dos(:,1), -sum_dos(:,3),'r','LineWidth', 2)
-        patch([sum_dos(ind,1);flipud(sum_dos(ind,1))], ...
-            [sum_dos(ind,2);flipud(zeros(length(ind(ind)),1))],...
+        
+        sum_dos_up = sum_dos;tmp = sum_dos_up(:,1) - sum_dos_up(ind_up(1)-1,1);
+        ind = tmp <= 0;
+        
+        
+        patch([sum_dos_up(ind,1);flipud(sum_dos_up(ind,1))], ...
+            [sum_dos_up(ind,2);flipud(zeros(length(ind(ind)),1))],...
             [45 48 52]/255,...
             'FaceA',.2,'EdgeA',0);
-        patch([sum_dos(ind,1);flipud(sum_dos(ind,1))], ...
-            [-sum_dos(ind,3);flipud(zeros(length(ind(ind)),1))],...
-            [45 48 52]/255,...
+        
+        
+        sum_dos_down = sum_dos;tmp = sum_dos_down(:,1) - sum_dos_up(ind_down(1)-1,1);
+        ind = tmp <= 0;
+        
+        patch([sum_dos_down(ind,1);flipud(sum_dos_down(ind,1))], ...
+            [-sum_dos_down(ind,3);flipud(zeros(length(ind(ind)),1))],...
+            [45 48 52]/255,...axis([-5.5 3.5, -40 40])
             'FaceA',.2,'EdgeA',0);
         for ik = 1:2*length(atom)+1; tmp_atom{ik} = 'a';end
         tmp_atom{1} = [atom{1},'_{up}'];
@@ -101,7 +116,7 @@ if have_pdos
         end
         ind = sum_dos(:,1) <= 0;
         plot(sum_dos(:,1), sum_dos(:,2),'r','LineWidth', 2)
-        patch([sum_dos(ind,1);flipud(sum_dos(ind,1))], ...
+        patch([sum_dos(ind,1);flipud(sum_dos(ind,1))], ...axis([-5 3.5, -40 40])
             [sum_dos(ind,2);flipud(zeros(length(ind(ind)),1))],...
             [45 48 52]/255,...
             'FaceA',.2,'EdgeA',0);
@@ -111,5 +126,5 @@ if have_pdos
     title(['DOS of ',deblank(sys_name)],'fontsize',18)
 end
 yval = get(gca, 'ylim');
-text(0,1.1*yval(1),'E_{fermi}')
-line([0, 0],[yval(1) yval(end)], 'linestyle','--')
+text(s(4),1.1*yval(1),'E_{fermi}')
+line([s(4), s(4)],[yval(1) yval(end)], 'linestyle','--')
