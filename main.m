a7332f78dbf19a4aa4cb4c478c4bbd538e47df0d
ChangChunHe <731176792@qq.com>
2018-11-09 19:59:06 +0800

    add example

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 clear
 clc
-pat = 'MgH2';
+pat = 'PtSe';
 have_pdos = true;
 eigval_file = fullfile(pat, 'EIGENVAL');
 dos_file = fullfile(pat, 'DOSCAR');
@@ -9,8 +9,8 @@ kpts_file = fullfile(pat, 'KPOINTS');
 draw_band_structure_dos(eigval_file, dos_file, pos_file, kpts_file)
 draw_band_structure(eigval_file, pos_file, kpts_file)
 figure
-%draw_dos_element(dos_file, pos_file)
-% % axis([-4.5 5 -40 40])
+draw_dos_element(dos_file, pos_file)
+% % % axis([-4.5 5 -40 40])
 figure
 draw_dos_pdos(dos_file, pos_file)
 % axis([-4.5 5 -40 40])
