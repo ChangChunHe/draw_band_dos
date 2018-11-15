b9d5a8376f3d8d3164413b2dc5a412796c9e538c
ChangChunHe <731176792@qq.com>
2018-11-15 21:16:54 +0800

    use regexp to mathch positions

--- read_poscar.m
+++ read_poscar.m
@@ -20,7 +20,21 @@ end
 temp_str = str2num(temp_str{1});
 
 fid = fopen(filename,'rt');
-S = cell2mat(textscan(fid, '%f %f %f %*s', sum(temp_str), 'Headerlines',k+1));
+S = zeros(sum(temp_str),3);
+iter = 1;
+while feof(fid) == 0
+    tline = fgetl(fid);
+    if iter > k+2
+        t = regexp(tline,'[+-]?\d+\.?\d*', 'match');
+        S(iter-k-2,:) = [str2double(t{1}) str2double(t{2}) str2double(t{3})];
+        
+    end
+    iter = iter + 1;
+    if iter > k+sum(temp_str)+2
+        break
+    end
+end
+
 fclose(fid);
 
 
@@ -39,7 +53,7 @@ if length(a) ~= length(temp_str)
     error('Atom species are not consistent to atom numbers')
 end
 for ii = 1:length(a)
-   atom = [atom repmat([a{ii}, ' '],1, temp_str(ii))]; 
+    atom = [atom repmat([a{ii}, ' '],1, temp_str(ii))];
 end
 
 
