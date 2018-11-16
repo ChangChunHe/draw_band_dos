54bf742a9b190aacab7dd0a2c322fd577d187b62
ChangChunHe <731176792@qq.com>
2018-11-16 15:18:38 +0800

    add selective option

--- read_poscar.m
+++ read_poscar.m
@@ -2,35 +2,35 @@ function [C, S, atom] = read_poscar(filename)
 fid = fopen(filename, 'rt');
 C = cell2mat(textscan(fid, '%f %f %f', 3, 'Headerlines',2));
 fclose(fid);
-
 fid = fopen(filename, 'rt');
 k = 1;
 while feof(fid) == 0
     tline = fgetl(fid);
-    
     if k == 7
         temp_str=regexp(tline,'(\d\s*)*','match'); %cell
-        break
     elseif k == 8
-        temp_str=regexp(tline,'(\d\s*)*','match'); %cell
+        select_flag=regexp(tline,'elective','match'); %cell
         break
+    elseif k == 9
+        
     end
     k = k +1;
 end
 temp_str = str2num(temp_str{1});
+if isempty(select_flag); headline = 8;else headline = 9;end
 
 fid = fopen(filename,'rt');
 S = zeros(sum(temp_str),3);
 iter = 1;
 while feof(fid) == 0
     tline = fgetl(fid);
-    if iter > k+2
+    if iter > headline
         t = regexp(tline,'[+-]?\d+\.?\d*', 'match');
-        S(iter-k-2,:) = [str2double(t{1}) str2double(t{2}) str2double(t{3})];
+        S(iter-headline,:) = [str2double(t{1}) str2double(t{2}) str2double(t{3})];
         
     end
     iter = iter + 1;
-    if iter > k+sum(temp_str)+2
+    if iter > sum(temp_str)+headline
         break
     end
 end
